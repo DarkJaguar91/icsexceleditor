5c36693d7f110ea6f67ef168b9cef33c9a6520ea
bjtal91@gmail.com <bjtal91@gmail.com@bd2b6f96-f12e-9449-da49-b49a147d6ea5>
2013-04-09 12:35:17 +0000

    Initial import.

---
+++ trunk/ImageEditor/src/GUI/MainForm.java
@@ -0,0 +1,16 @@
+package GUI;
+
+import javax.swing.JFrame;
+
+public class MainForm extends JFrame {
+	
+	/**
+	 * Generated Serial ID
+	 */
+	private static final long serialVersionUID = -1235861920996215785L;
+
+	public MainForm(){
+		
+	}
+	
+}
---
+++ trunk/ImageEditor/src/runner/Runner.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package runner;
+
+import GUI.MainForm;
+
+/**
+ * @author Brandon
+ *
+ */
+public class Runner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new MainForm();
+	}
+
+}
