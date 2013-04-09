4f4adb9560c88e005f94ea97d53ee4828c20d0c5
bjtal91@gmail.com <bjtal91@gmail.com@bd2b6f96-f12e-9449-da49-b49a147d6ea5>
2013-04-09 19:13:01 +0000

    update to ICSEXCELEDITOR -- Changed the date read to use ETA rather than ETD
    update to image editor -- allot -> fixed the equalization

--- ImageEditor/src/GUI/MainForm.java
+++ ImageEditor/src/GUI/MainForm.java
@@ -1,6 +1,22 @@
 package GUI;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.BevelBorder;
+
+import Image.ImageHolder;
+import Image.ImageProcessor;
 
 public class MainForm extends JFrame {
 	
@@ -9,8 +25,43 @@ public class MainForm extends JFrame {
 	 */
 	private static final long serialVersionUID = -1235861920996215785L;
 
+	JPanel p, p2, cont;
+	ImageHolder holder, holder2;
+	
 	public MainForm(){
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
+		setSize(800, 600);
+		
+		holder = new ImageHolder(new ImageIcon("input.jpg").getImage());
+		holder2 = ImageProcessor.equalizeImage(holder);
+		
+		p = new JPanel(){
+			public void paint(Graphics g){
+				g.drawImage(holder.getImage(), 0, 0, this.getWidth(), this.getHeight(), null);
+			}
+		};
+		
+		p2 = new JPanel(){
+			public void paint(Graphics g){
+				g.drawImage(holder2.getImage(), 0, 0, this.getWidth(), this.getHeight(), null);
+			}
+		};
+		
+		this.setLayout(new BorderLayout());
+		
+		cont = new JPanel();
+		
+		p.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
+		p2.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
+		
+		cont.setLayout(new GridLayout(0, 2));
+		cont.add(p);
+		cont.add(p2);
+		
+		this.add(cont, BorderLayout.CENTER);
+		
+		setVisible(true);
 	}
 	
 }
---
+++ ImageEditor/src/Image/ImageHolder.java
@@ -0,0 +1,36 @@
+package Image;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.nio.Buffer;
+
+public class ImageHolder {
+	// globals
+	protected BufferedImage image;
+	
+	public ImageHolder(BufferedImage image){
+		this.image = image;
+	}
+	
+	public ImageHolder(Image inImage){
+		image = new BufferedImage(inImage.getWidth(null), inImage.getHeight(null), BufferedImage.TYPE_INT_RGB);
+		
+		image.getGraphics().drawImage(inImage, 0, 0, image.getWidth(), image.getHeight(), null);		
+	}
+	
+	public Image getImage(){
+		return image.getScaledInstance(image.getWidth(), image.getHeight(), BufferedImage.SCALE_SMOOTH);
+	}
+	
+	public BufferedImage getBufferedImage(){
+		return image;
+	}
+	
+	public int getWidth(){
+		return image.getWidth();
+	}
+	
+	public int getHeight(){
+		return image.getHeight();
+	}
+}
