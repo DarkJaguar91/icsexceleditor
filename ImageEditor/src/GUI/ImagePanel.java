f9f58aa96a4b4113196e11743230761e4a9ddf4a
bjtal91@gmail.com <bjtal91@gmail.com@bd2b6f96-f12e-9449-da49-b49a147d6ea5>
2013-04-09 12:35:17 +0000

    Initial import.

---
+++ ImageEditor/src/GUI/ImagePanel.java
@@ -0,0 +1,19 @@
+package GUI;
+
+import javax.swing.JInternalFrame;
+
+public class ImagePanel extends JInternalFrame{
+	
+	/**
+	 * Generated Serial ID
+	 */
+	private static final long serialVersionUID = 4201844286968346886L;
+
+	// Globals
+	
+	
+	public ImagePanel (){
+		
+	}
+	
+}
